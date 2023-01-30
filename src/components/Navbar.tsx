c97f6e2fa984ee69ad80d7b2a6e9774c76566564
ilyas Macbook Air <ilyasmgazali@gmail.com>
2023-01-30 20:51:29 +0000

    project set up, Home+Nav edit

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,16 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    height: 60px;
+    background-color: #333;
+    color: white;
+`;
+
 export default function Navbar() {
-    return <div>Navbar</div>;
+    return (
+        <>
+            <Container>Logo</Container>
+        </>
+    );
 }
